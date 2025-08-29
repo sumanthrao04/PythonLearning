28c5e56ae749169da85f9c12d2f9dedb6127793e
sumanthrao04 <sumanthrao75@gmail.com>
2025-08-29 15:11:05 +0530

    Hello world

---
+++ Hello.py
@@ -0,0 +1,7 @@
+
+# Python Indentation
+# Indentation refers to the spaces at the beginning of a code line.
+# Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+# Python uses indentation to indicate a block of code.
+print("Hello, World!")
+print("This is a simple Python script.")
